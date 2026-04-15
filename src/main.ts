012ab44d59994e6f8dc0028bdd5e2d84a26dcd25
jothinathan423 <jothinathannagarajan@gmail.com>
2026-04-15 12:28:44 +0530

    fix: DNS issue fixed

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import * as dns from 'dns';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
