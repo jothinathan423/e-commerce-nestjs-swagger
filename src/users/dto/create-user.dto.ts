546b3f77321a10574a58f44ae664cd36022f16c4
jothinathan423 <jothinathannagarajan@gmail.com>
2026-04-18 10:51:19 +0530

    `

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,19 +1,6 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-
 export class CreateUserDto {
-  @ApiProperty({ type: String, example: 'Jothinathan' })
   name!: string;
-
-  @ApiProperty({ type: String, example: 'jothi@example.com' })
   email!: string;
-
-  @ApiProperty({ type: String, example: 'secret123' })
   password!: string;
-
-  @ApiPropertyOptional({
-    type: String,
-    enum: ['customer', 'admin'],
-    default: 'customer',
-  })
   role?: 'customer' | 'admin';
 }
